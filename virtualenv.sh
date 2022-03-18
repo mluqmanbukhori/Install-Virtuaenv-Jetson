895d27829fc14580e179ec0bd69b84c36c6271d2
Muhammad Luqman Bukhori <44484883+mluqmanbukhori@users.noreply.github.com>
2022-03-18 13:24:27 +0700

    Create virtualenv.sh

---
+++ virtualenv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Install Virtual Environment di Jetson Development Kits"
+# Install virtualenv dan virtualenvwrapper
+sudo pip3 install virtualenv virtualenvwrapper
+
+# Edit bashrc file
+echo '# virtualenv and virtualenvwrapper script' >> ~/.bashrc
+echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
+echo 'export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3' >> ~/.bashrc
+echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+
+# Compiling bashrc file
+source ~/.bashrc
+
+# Buat nama directory virtualenv. Ganti "ai" dengan nama virtualenv yang diinginkan 
+mkvirtualenv ai -p python3
+
+# Copy OpenCV global ke virtualenv
+cd ~/.virtualenvs/ai/lib/python3.6/site-packages/
+ln -s /usr/lib/python3.6/dist-packages/cv2/python-3.6/cv2.cpython-36m-aarch64-linux-gnu.so cv2.s
+
+echo "Selamat ..."
+echo "Virtual Environment sudah terinstall!"
